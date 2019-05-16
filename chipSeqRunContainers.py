d5b305e66971737ea99fce3c5fed3c41f8b7b1cb
NachoDave <davidwjames83@gmail.com>
2019-05-16 14:23:47 +0100

    Both paired and unpaired classes for Bowtie2 working

--- chipSeqRunContainers.py
+++ chipSeqRunContainers.py
@@ -58,7 +58,7 @@ class runBowtie2Unpaired:
         print(self.targetFNOut + self.ctrlFNOut)
         print('\n')
 
-        with open(self.logDr + '/' + 'Align' + self.dt + '.err', 'w+') as lgf:
+        with open(self.logDr + '/' + 'AlignBowtie2Unpaired' + self.dt + '.err', 'w+') as lgf:
 
             lgf.write('Align stderr ' + str(datetime.datetime.now()) + '\n') # open log file for writing
             for fqFN, fqFNOut in zip(self.targetFN + self.ctrlFN, self.targetFNOut + self.ctrlFNOut): # loop trhough files
@@ -84,8 +84,8 @@ class runBowtie2Unpaired:
                             '-S', '/data/outputFiles/' + fqFNOut
                     ]
 
-                    #if bwt2ArgsN: # if there are command line args add these in
-                    #    bwt2ParLst[len(bwt2ParLst):len(bwt2ParLst) + bwt2ArgsN] = inPars['bowtie2Args']
+                    if self.args: # if there are command line args add these in
+                        bwt2ParLst[len(bwt2ParLst):len(bwt2ParLst) + len(self.args)] = self.args
                         #print(bwt2ParLst)
                     p = subprocess.Popen(bwt2ParLst,
                     shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE) # run process
@@ -95,10 +95,10 @@ class runBowtie2Unpaired:
                     print('err:' + err)
 
                     lgf.write('\nFile to align: ' +   inPth + '\nGenome:'
-                    + self.genomePth + self.genome + '\nOutput File:' + outPth + '\n')
-
+                    + self.genomePth + self.genome + '\nOutput File:' + outPth + '\n'
+                    + 'Docker/Bowtie2 arguments: ' + ','.join(bwt2ParLst))
                     if self.args:
-                        lgf.write('Cmd line Args: ' + self.args + '\n\n')
+                        lgf.write('Cmd line Args: ' + ','.join(self.args) + '\n\n')
                     else:
                         lgf.write('No additional command line args\n\n')
                     lgf.write(err)
@@ -108,7 +108,7 @@ class runBowtie2Unpaired:
 
         lgf.close()
 
-
+"""=========================================================================="""
 ''' Class to run docker container for Paired bowtie2 ========================'''
 class runBowtie2Paired:
     ''' Variables:
@@ -139,7 +139,7 @@ class runBowtie2Paired:
         if targetFNOut is None:
             targetFNOut = [w.replace('.fastq', '_bowtie2.sam').replace('.gz', '') for w in targetFN1]
         if ctrlFNOut is None:
-            if ctrlFN:
+            if ctrlFN1:
                 ctrlFNOut = [w.replace('.fastq', '_bowtie2.sam').replace('.gz', '') for w in ctrlFN1]
             else:
                 ctrlFNOut = []
@@ -160,38 +160,42 @@ class runBowtie2Paired:
     ''' main function to run container --------------------------------------'''
 
     def run(self):
-        print(self.targetFN1 + self.ctrlFN)
-        print(self.targetFNOut + self.ctrlFNOut)
+        print(self.targetFN1 + self.ctrlFN1, '\n')
+        print(self.targetFN2 + self.ctrlFN2, '\n')
+        print(self.targetFNOut + self.ctrlFNOut, '\n')
         print('\n')
 
-        with open(self.logDr + '/' + 'Align' + self.dt + '.err', 'w+') as lgf:
+        with open(self.logDr + '/' + 'AlignBowtie2Paired' + self.dt + '.err', 'w+') as lgf:
 
             lgf.write('Align stderr ' + str(datetime.datetime.now()) + '\n') # open log file for writing
-            for fqFN, fqFNOut in zip(self.targetFN1 + self.ctrlFN1, self.targetFNOut + self.ctrlFNOut): # loop trhough files
+            for fqFN, fqFN2, fqFNOut in zip(self.targetFN1 + self.ctrlFN1, self.targetFN2 + self.ctrlFN2,self.targetFNOut + self.ctrlFNOut): # loop trhough files
 
                 inPth = self.inDr + '/' + fqFN
+                inPth2 = self.inDr + '/' + fqFN2
                 outPth = self.outDr + '/' + fqFNOut
                 print('\n')
-                print('Locating file:', inPth, '\n')
+                print('Locating files:', inPth, 'and', inPth2, '\n')
                 print('Output path', outPth, '\n')
                 print('\n')
 
-                if os.path.exists(inPth):
-                    print(inPth, 'exists, attempting alignment')
+                if os.path.exists(inPth) and os.path.exists(inPth2):
+                    print(inPth, 'and', inPth2, 'paired reads exist, attempting alignment')
 
                     ''' Input to bowtie2 -----------------------------------'''
+
                     bwt2ParLst = ['docker', 'run','--rm','-v', self.genomePth + ':/data/genomes/',
                             '-v', self.inDr + ':/data/inputFiles',
                             '-v', self.outDr + ':/data/outputFiles',
                             'biocontainers/bowtie2:v2.2.9_cv2',
                             'bowtie2', '-q',
                             '-x', self.genomePth + self.genome,
-                            '-U', '/data/inputFiles/' + fqFN,
+                            '-1', '/data/inputFiles/' + fqFN,
+                            '-2', '/data/inputFiles/' + fqFN2,
                             '-S', '/data/outputFiles/' + fqFNOut
                     ]
 
-                    #if bwt2ArgsN: # if there are command line args add these in
-                    #    bwt2ParLst[len(bwt2ParLst):len(bwt2ParLst) + bwt2ArgsN] = inPars['bowtie2Args']
+                    if self.args: # if there are command line args add these in
+                        bwt2ParLst[len(bwt2ParLst):len(bwt2ParLst) + len(self.args)] = self.args
                         #print(bwt2ParLst)
                     p = subprocess.Popen(bwt2ParLst,
                     shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE) # run process
@@ -200,11 +204,11 @@ class runBowtie2Paired:
                     print('out:' + output)
                     print('err:' + err)
 
-                    lgf.write('\nFile to align: ' +   inPth + '\nGenome:'
-                    + self.genomePth + self.genome + '\nOutput File:' + outPth + '\n')
-
+                    lgf.write('\nPaired Files to align: ' +   inPth + ' and ' + inPth2 + '\nGenome:'
+                    + self.genomePth + self.genome + '\nOutput File:' + outPth + '\n'
+                    + 'Docker/Bowtie2 arguments: ' + ','.join(bwt2ParLst))
                     if self.args:
-                        lgf.write('Cmd line Args: ' + self.args + '\n\n')
+                        lgf.write('Cmd line Args: ' + ','.join(self.args) + '\n\n')
                     else:
                         lgf.write('No additional command line args\n\n')
                     lgf.write(err)
@@ -213,3 +217,6 @@ class runBowtie2Paired:
                     print('Could not find file', inPth, 'skipping alignment')
 
         lgf.close()
+
+"""=========================================================================="""
+''' Class to run docker container for MACS =================================='''
--- testRunContainers.py
+++ testRunContainers.py
@@ -7,17 +7,31 @@ import chipSeqRunContainers as cont
 """ Bowtie2 unpaired aligner ------------------------------------------------"""
 
 tarFN = ['SRR6730206_shrt_RndSamp.fastq', 'SRR6730206_shrt_RndSampCopy.fastq']
-ctrlFN = ['SRR6730208_1000bpRnd.fastq', 'xxx']
+tarFN2 = ['SRR6730206_shrt_RndSamp2.fastq', 'SRR6730206_shrt_RndSampCopy2.fastq']
+ctrlFN = ['SRR6730208_1000bpRnd.fastq']
+ctrlFN2 = ['SRR6730208_1000bpRnd2.fastq']
 inDr = '/data/ChIPSeqAnalysis/Experiments/e/data/'
 genPth = '/data/genomes/'
 resDr = '/data/ChIPSeqAnalysis/Experiments/e/results/test'
 genome = 'hg19bowtie2'
+args = ["-p4", "-t"]
+logDr = resDr + '/logs'
 
-#b = bwt2UP(inDr, tarFN, genPth, 'x')
-b = cont.runBowtie2Unpaired(inDr, tarFN, genPth, genome, ctrlFN = ctrlFN, outDr = resDr)
+
+b = cont.runBowtie2Unpaired(inDr, tarFN, genPth, genome, ctrlFN = ctrlFN, outDr = resDr, args = args, logDr = logDr)
 #print(b.inDr,  b.targetFN,  b.genomePth,  b.targetFNOut, b.ctrlFN, b.outDr, b.logDr)
-print(b.targetFN)
-print('\n')
-print(b.ctrlFN)
-print('\n')
+#print(b.targetFN)
+#print('\n')
+#print(b.ctrlFN)
+#print('\n')
 b.run()
+'''
+c = cont.runBowtie2Paired(inDr, tarFN, tarFN2, genPth, genome, ctrlFN1 = ctrlFN, ctrlFN2 = ctrlFN2,outDr = resDr, args = args)
+print(c.inDr,  c.targetFN1,  c.targetFN2,c.genomePth,  c.targetFNOut, c.ctrlFN1, c.ctrlFN2,c.outDr, c.logDr)
+print('\n')
+print(c.targetFN1)
+print('\n')
+print(c.ctrlFN1)
+print('\n')
+c.run()
+'''
