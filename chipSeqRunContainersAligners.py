8dc2ac7a9b5d7dee8e55bb5100bc53947c31af7e
NachoDave <davidwjames83@gmail.com>
2019-05-17 18:59:58 +0100

    Added seperate files for aligner classes and SAMtools class

--- chipSeqRunContainersAligners.py
+++ chipSeqRunContainersAligners.py
@@ -218,54 +218,6 @@ class runBowtie2Paired:
 
         lgf.close()
 
-"""=========================================================================="""
-''' Class to run docker container for bedtools =============================='''
-
-class runSamtools:
-
-    def __init__(self, inDr, targetFN, logDr = None, ctrlFN = [],  targetFNOut = None, ctrlFNOut = None):
-        # Write BAM files to the same directory as the SAM files
-        self.inDr = inDr
-        self.targetFN = targetFN
-        self.ctrlFN = ctrlFN
-        if logDr is None:
-            logDr = inDr
-        if targetFNOut is None:
-            targetFNOut = [w.replace('.sam', '.bam') for w in targetFN]
-        if ctrlFNOut is None:
-            ctrlFNOut = [w.replace('.sam', '.bam') for w in ctrlFN]
-
-        self.targetFNOut = targetFNOut
-        self.ctrlFNOut = ctrlFNOut
-        self.logDr = logDr
-        dt = str(datetime.datetime.now())
-        dt = dt[0:10]
-        dt = dt.replace('-', '')
-        self.dt = dt
-
-
-    ''' Method to convert sam files to bam files '''
-    def sam2Bam(self):
-        print(self.targetFNOut)
-        print('\n')
-        print(self.ctrlFNOut)
-        samViewErr = open(self.logDr + '/samView' + self.dt + '.err', 'w+')
-        samViewErr.write('Error log for samtools view ' + self.dt)
-
-        for inNm, outNm in zip(self.targetFN + self.ctrlFN, self.targetFNOut + self.ctrlFNOut):
-            print(self.inDr, inNm, outNm)
-            print('\n')
-
-        samViewErr.close()
-    ''' Method to create BAM index '''
-    #def indexBam(self):
-
-    ''' Method to sort BAM files '''
-    #def sortBam(self):
-
-    ''' Run all 3 methods to convert, index and sort SAM '''
-    def run(self):
-        self.sam2Bam()
 
 """=========================================================================="""
 ''' Class to run docker container for MACS =================================='''
